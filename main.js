acc7199cb075111d5cb321a3d48fc4c18314f73c
Thomas Ferullo <thomasferullo@Andrews-MacBook-Pro.local>
2018-07-11 11:55:03 -0400

    added guessed letters

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ window.onload=function(){
     let buttonWord = document.querySelector("#submit-word")
     let inputLetter = document.querySelector("#letter-box")
     let buttonLetter = document.querySelector("#submit-letter")
-
+    let guessedLetters = document.querySelector("#guessedLetters")
+    
     buttonWord.addEventListener('click', function(event) {
+        let guessedLettersArray = []
         let inputWordArray = inputWord.value.split("")
         for (i = 0; i < inputWordArray.length; i++) {
             let box = document.createElement('div')
@@ -14,6 +16,12 @@ window.onload=function(){
             buttonLetter.addEventListener('click', function(event){
                 if (inputLetter.value === boxLetter) {
                     box.innerHTML = boxLetter
+                } else {
+                    guessedLettersArray.push(inputLetter.value)
+                    let UguessedLettersArray = guessedLettersArray.filter(function(elem, index, self){
+                        return index == self.indexOf(elem);
+                    })
+                    guessedLetters.innerHTML = UguessedLettersArray
                 }
                 event.preventDefault()
             })
