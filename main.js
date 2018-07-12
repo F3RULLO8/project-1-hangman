d932ecebe1fc0223ad28e96e9b78f8258aa8de88
Thomas Ferullo <thomasferullo@Andrews-MacBook-Pro.local>
2018-07-12 16:07:33 -0400

    fixed so now u can win again

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ window.onload=function(){
                         usedLetterArray.push(inputLetter.value)
                         box.innerHTML = boxLetter
                         winScore = winScore + 1
-                    }
+                    } else {
                     guessedLettersArray.push(inputLetter.value)
                     lossScore = lossScore + 1
                     let UguessedLettersArray = guessedLettersArray.filter(function(elem, index, self){
@@ -50,6 +50,7 @@ window.onload=function(){
                     }
                     guessedLetters.innerHTML = UguessedLettersArray
                 }
+                }   
                 stickFigure (lossScore)
                 event.preventDefault()
             })
